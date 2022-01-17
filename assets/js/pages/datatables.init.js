0a907762c8eda69c4b45dc4963c452f66a02913a
mainuddin-sani <main12sani@gmail.com>
2022-01-17 23:44:18 +0600

    admin panel design

---
+++ assets/js/pages/datatables.init.js
@@ -0,0 +1,12 @@
+!(function () {
+  $(document).ready(function () {
+    $(".cc-datatable").DataTable({
+      language: {
+        paginate: {
+          next: '<i class="icon-right-arrow"></i>',
+          previous: '<i class="icon-left-arrow"></i>',
+        },
+      },
+    });
+  });
+})();
