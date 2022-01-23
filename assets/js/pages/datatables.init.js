208188ae448d28bd7211aeaf39927a1f6a7bedd6
mainuddin-sani <main12sani@gmail.com>
2022-01-24 00:35:59 +0600

    datatable change

--- assets/js/pages/datatables.init.js
+++ assets/js/pages/datatables.init.js
@@ -1,13 +1,19 @@
 !(function () {
   $(document).ready(function () {
     $(".cc-datatable").DataTable({
+      lengthChange: !1,
+      buttons: ["copy", "excel", "pdf", "colvis"],
       language: {
+       
         paginate: {
           next: '<i class="icon-right-arrow"></i>',
           previous: '<i class="icon-left-arrow"></i>',
         },
+        searchPlaceholder: "Search",
+        search: '<i class="fas fa-search"></i>',
       },
     });
+
     $(".dataTable").wrap('<div class="table-responsive"><div>');
   });
 })();
--- assets/js/pages/morris.init.js
+++ assets/js/pages/morris.init.js
@@ -1,17 +1,17 @@
-(function ($) {
-  "use strict";
+// (function ($) {
+//   "use strict";
 
-  Morris.Donut({
-    element: "morris_donught",
-    data: [
-      { label: "Friends", value: 30 },
-      { label: "Allies", value: 15 },
-      { label: "Enemies", value: 45 },
-      { label: "Neutral", value: 10 },
-    ],
-    resize: true,
-    redraw: true,
-    colors: ["#461EE7", "rgb(30, 170, 231)", "#2BC155"],
-    //responsive:true,
-  });
-})(jQuery);
+//   Morris.Donut({
+//     element: "morris_donught",
+//     data: [
+//       { label: "Friends", value: 30 },
+//       { label: "Allies", value: 15 },
+//       { label: "Enemies", value: 45 },
+//       { label: "Neutral", value: 10 },
+//     ],
+//     resize: true,
+//     redraw: true,
+//     colors: ["#461EE7", "rgb(30, 170, 231)", "#2BC155"],
+//     //responsive:true,
+//   });
+// })(jQuery);
